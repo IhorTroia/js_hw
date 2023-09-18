04b8804d7b6e4ea585b58387740378b609f17888
Ihor Troianovskyi <igtroya555@gmail.com>
2023-09-18 13:13:45 +0300

    hw 1 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
 'use strict';
+
+let admin;
+let name;
+
+name = "John";
+admin = name;
+
+alert(admin);
+
+let ourPlanet;
+let userName;
+
+const sum = '10' + 10;
+
+console.log(typeof(sum));
+
+
+
