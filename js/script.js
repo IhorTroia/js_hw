dbcd32aac948bd372e3b52cdcac7b87ca59570f7
Ihor Troianovskyi <igtroya555@gmail.com>
2023-09-19 15:38:29 +0300

    hw 4 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const calc = prompt('select add, sub, mult or div');
+const firstOperand = +prompt('select first operand');
+const  secondOperand = +prompt('select second one');
+
+function promptCalc() {
+    if(calc === 'add') {
+        alert(firstOperand + secondOperand);
+    } else if(calc === 'sub') {
+        alert(firstOperand - secondOperand);
+    } else if(calc === 'mult') {
+        alert(firstOperand * secondOperand);
+    } else if(calc === 'div') {
+        alert(firstOperand / secondOperand);
+    } else {
+        alert('enter number please')
+    }
+}
+
+promptCalc();
