64e4c60162861cf0e07c24d346df8aa8da2fc020
Ihor Troianovskyi <igtroya555@gmail.com>
2023-09-19 15:22:15 +0300

    hw 3 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const firstOperand = +prompt('Enter first number');
+const secondOperand = +prompt('Enter second one');
+
+function sumCalc() {
+    return console.log(firstOperand + secondOperand);
+}
+function subtractCalc() {
+    return console.log(firstOperand - secondOperand);
+}
+function multiplyCalc() {
+    return console.log(firstOperand * secondOperand);
+}
+function divisionCalc() {
+    return console.log(firstOperand / secondOperand);
+}
+
+sumCalc();
+subtractCalc();
+multiplyCalc();
+divisionCalc();
+
+
+
+
+
