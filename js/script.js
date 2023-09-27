7172e4bc1688477569247e6c207890bc89287c43
Ihor Troianovskyi <igtroya555@gmail.com>
2023-09-27 13:24:03 +0300

    hw 8 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,36 @@
 'use strict';
+
+let user = {};
+
+user.name = 'John';
+user.surname = 'Smith';
+
+user.name = 'Pete';
+
+delete user.name;
+
+console.log(user);
+
+// ---------
+
+const userTest = {
+    name: "John"
+};
+
+// Це працюватиме?
+
+userTest.name = "Pete";
+
+console.log(userTest);
+
+// Это работает потому что обьект это ссылочный тип
+// --------
+
+let salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130
+}
+
+const sum = salaries.John + salaries.Ann + salaries.Pete;
+console.log(sum);
