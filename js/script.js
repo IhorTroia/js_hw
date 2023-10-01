d4baa275351f2e5d1a5d86df75e80d4aa66eb803
Ihor Troianovskyi <igtroya555@gmail.com>
2023-10-01 09:35:37 +0300

    hw 11 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
 'use strict';
+
+let num = 1;
+num = num + 12;
+num = num - 14;
+num = num * 5;
+num = num/7;
+num = num + 1;
+num = num - 1;
+alert(num);
+// ------------
+let numHW = 1;
+numHW += 12;
+numHW -= 14;
+numHW *= 5;
+numHW /= 7;
+numHW = ++numHW;
+numHW = --numHW;
+alert(numHW);
+
+
