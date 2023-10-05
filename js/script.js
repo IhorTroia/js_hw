965db65468b648d2e2e12520140308d60134f02f
Ihor Troianovskyi <igtroya555@gmail.com>
2023-10-05 14:41:13 +0300

    hw 14 completed

--- js/script.js
+++ js/script.js
@@ -1,14 +1,18 @@
-'use strict';
+'use strict'
 
-let numOrStr = prompt('input number or string');
-console.log(numOrStr)
+let numOrStrHW = prompt(`input number or string`);
+console.log(numOrStrHW);
 
-if(numOrStr === null) {
-    console.log('ви скасували')
-} else if( numOrStr.trim() === '' ) {
-    console.log('Empty String');
-} else if ( isNaN( +numOrStr ) ) {
-    console.log(' number is Ba_NaN')
-} else {
-    console.log('OK!')
+switch (true) {
+    case (numOrStrHW === null):
+        console.log('ви скасували');
+        break;
+    case (numOrStrHW.trim() === ''):
+        console.log('Empty String');
+        break;
+    case (isNaN(+numOrStrHW)):
+        console.log(' number is Ba_NaN');
+        break;
+    default:
+        console.log('OK!');
 }
