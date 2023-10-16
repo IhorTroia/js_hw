ab5503ea8f943171757b88e43ecf780d458370f7
Ihor Troianovskyi <igtroya555@gmail.com>
2023-10-16 18:25:35 +0300

    hw 20 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
-'use strict';
+'use strict'
+
+const characters = 'abcdefghijklmnopqrstuvwxyz0123456789'
+
+function generateKey(length, characters) {
+    let result = ''
+    for (let i = 0; i < length; i++) {
+        const randomValue = Math.floor(Math.random() * characters.length)
+        result += characters[randomValue]
+    }
+    return result
+}
+
+const key = generateKey(16, characters)
+console.log(key)
