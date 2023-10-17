da1d1a8002c42f3bbd7e43d3746d9a05f32adcaf
Ihor Troianovskyi <igtroya555@gmail.com>
2023-10-17 14:49:40 +0300

    hw 22 updated

--- js/script.js
+++ js/script.js
@@ -1,28 +1,26 @@
-'use strict'
-
-function padString (word, length, symb, bool = true) {
-    if (typeof word !== 'string') {
-        return 'error: no word argument'
-    } else if (typeof length !== 'number') {
-        return 'error: no length argument'
-    } else if (typeof symb !== 'string' || symb.length !== 1) {
-        return 'error: no symb argument'
-    } else if (typeof bool !== 'boolean') {
-        return "error: no bool argument"
-    } else if (word.length > length) {
-        word =  word.substring(0, length)
-    } else {
-        word = Array.from(word)
-        for (let i = word.length; i < length; i++) {
-            if (bool === false) {
-                word.unshift(symb)
-            } else {
-                word.push(symb)
-            }
-        }
-        word = word.join('')
+function padString(word, length, symb, bool = true) {
+  if (typeof word !== 'string') {
+    return 'error: no word argument';
+  } if (typeof length !== 'number') {
+    return 'error: no length argument';
+  } if (typeof symb !== 'string' || symb.length !== 1) {
+    return 'error: no symb argument';
+  } if (typeof bool !== 'boolean') {
+    return 'error: no bool argument';
+  } if (word.length > length) {
+    word = word.substring(0, length);
+  } else {
+    word = Array.from(word);
+    for (let i = word.length; i < length; i++) {
+      if (bool === false) {
+        word.unshift(symb);
+      } else {
+        word.push(symb);
+      }
     }
-    return word
+    word = word.join('');
+  }
+  return word;
 }
 
-console.log(padString('hello', 6,'*', true))
+console.log(padString('hello', 6, '*', true));
