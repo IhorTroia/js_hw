239564c8116ec46cdf035756f9002cd4b116fde6
Ihor Troianovskyi <igtroya555@gmail.com>
2023-10-17 14:45:10 +0300

    hw 20 updated

--- js/script.js
+++ js/script.js
@@ -1,15 +1,13 @@
-'use strict'
+const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
-const characters = 'abcdefghijklmnopqrstuvwxyz0123456789'
-
-function generateKey(length, characters) {
-    let result = ''
-    for (let i = 0; i < length; i++) {
-        const randomValue = Math.floor(Math.random() * characters.length)
-        result += characters[randomValue]
-    }
-    return result
+function generateKey(length, character) {
+  let result = '';
+  for (let i = 0; i < length; i++) {
+    const randomValue = Math.floor(Math.random() * character.length);
+    result += character[randomValue];
+  }
+  return result;
 }
 
-const key = generateKey(16, characters)
-console.log(key)
+const key = generateKey(16, characters);
+console.log(key);
