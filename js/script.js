d48cddbf92be98173a6f2b2d123dffe790549384
Ihor Troianovskyi <igtroya555@gmail.com>
2023-10-18 11:57:58 +0300

    hw 24 updated

--- js/script.js
+++ js/script.js
@@ -1,1 +1,30 @@
-'use strict';
+const mainFoo = (callback) => {
+  const firstNum = prompt('enter some number');
+  const secondNum = prompt('enter power for num');
+  callback(firstNum, secondNum);
+};
+
+const exponentiationFoo = (a, b) => {
+  const res = a ** b;
+  alert(res);
+};
+
+const multiplyFoo = (a, b) => {
+  const res = a * b;
+  alert(res);
+};
+
+const divisionFoo = (a, b) => {
+  const res = a / b;
+  alert(res);
+};
+
+const moduloFoo = (a, b) => {
+  const res = a % b;
+  alert(res);
+};
+
+mainFoo(exponentiationFoo);
+mainFoo(multiplyFoo);
+mainFoo(divisionFoo);
+mainFoo(moduloFoo);
