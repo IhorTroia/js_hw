1460310eee17c4ab3280ba815067d84b4b57b889
Ihor Troianovskyi <igtroya555@gmail.com>
2023-11-06 17:42:18 +0200

    hw 30 completed

---
+++ js/script.js
@@ -0,0 +1,23 @@
+const array = [
+  1, 2, 3, 4, {},
+  [1, 2, 3],
+  [1, 2, 3],
+  [3, 4,
+    [5, 6, 7],
+  ],
+];
+
+function flat(arr) {
+  let res = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+      res = res.concat(flat(arr[i]));
+    } else {
+      res.push(arr[i]);
+    }
+  }
+  return res;
+}
+
+console.log(flat(array));
+console.log(flat(array));
