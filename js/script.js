0c49b474a76c78e2f321792ff5feee8b90f73662
Ihor Troianovskyi <igtroya555@gmail.com>
2023-12-08 14:22:06 +0200

    hw 41 upgraded

--- js/script.js
+++ js/script.js
@@ -1,25 +1,31 @@
-const gallery = document.getElementById('imageGallery');
-const modal = document.getElementById('modal');
-const modalImage = document.getElementById('modalImage');
-
-gallery.addEventListener('click', (event) => {
-  if (event.target.tagName === 'IMG') {
-    modalImage.src = event.target.src;
-    modal.style.display = 'flex';
-  }
-});
-
-modal.addEventListener('click', () => {
-  modal.style.display = 'none';
-});
-
-const showTab = (tabId) => {
-  const tabs = document.querySelectorAll('.tab-content');
-  tabs.forEach((tab) => {
-    if (tab.id === `${tabId}Content`) {
-      tab.classList.add('active');
-    } else {
-      tab.classList.remove('active');
+(() => {
+  const gallery = document.querySelector('[data-gallery]');
+  const modal = document.querySelector('[data-modal]');
+  const modalImg = document.querySelector('[data-modal-img]');
+  gallery.addEventListener('click', (e) => {
+    if (e.target.tagName === 'IMG') {
+      modalImg.src = e.target.src;
+      modal.style.display = 'flex';
     }
   });
-};
+  modal.addEventListener('click', () => {
+    modal.style.display = 'none';
+  });
+})();
+
+(() => {
+  const tabsItems = document.querySelectorAll('.tab');
+  const contentsItems = document.querySelectorAll('.tab-content');
+
+  console.log(tabsItems);
+  console.log(contentsItems);
+
+  tabsItems.forEach((tab, index) => {
+    tab.addEventListener('click', () => {
+      contentsItems.forEach((content) => {
+        content.classList.remove('active');
+      });
+      contentsItems[index].classList.add('active');
+    });
+  });
+})();
