8b7b149a0e30629a75204d064dc3041a98455530
Ihor Troianovskyi <igtroya555@gmail.com>
2023-12-18 18:57:00 +0200

    hw 50 AC

---
+++ js/ex.js
@@ -0,0 +1,77 @@
+(() => {
+  const createTemplate = (data) => {
+    const wrapper = document.createElement('div');
+    wrapper.className = 'col-4';
+    wrapper.setAttribute('data-todo-item', '');
+
+    const taskWrapper = document.createElement('div');
+    taskWrapper.className = 'taskWrapper';
+    wrapper.appendChild(taskWrapper);
+
+    const btnContainer = document.createElement('div');
+    btnContainer.className = 'text-end';
+    taskWrapper.appendChild(btnContainer);
+
+    const deleteBtn = document.createElement('button');
+    deleteBtn.className = 'btn btn-sm btn-danger';
+    deleteBtn.textContent = 'x';
+    deleteBtn.setAttribute('data-remove-btn', '');
+    btnContainer.appendChild(deleteBtn);
+
+    const taskHead = document.createElement('div');
+    taskHead.className = 'taskHeading';
+    taskHead.textContent = data.title;
+    taskWrapper.appendChild(taskHead);
+
+    const taskDesc = document.createElement('div');
+    taskDesc.className = 'taskDescription';
+    taskDesc.textContent = data.description;
+    taskWrapper.appendChild(taskDesc);
+
+    return wrapper;
+  };
+
+  const renderTodoItem = (elementToRender) => {
+    const todoContainer = document.querySelector('#todoItems');
+    todoContainer.prepend(elementToRender);
+
+    return elementToRender;
+  };
+
+  const formHandler = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    const { target } = e;
+
+    const inputData = {};
+
+    const inputs = target.querySelectorAll('input, textarea');
+    inputs.forEach((input) => {
+      if (input.value !== '') inputData[input.name] = input.value;
+    });
+
+    const HTMLTemplate = createTemplate(inputData);
+    renderTodoItem(HTMLTemplate);
+    target.reset();
+  };
+
+  const removeTodoItemHandler = (e) => {
+    e.stopPropagation();
+    const { target } = e;
+
+    if (!target.hasAttribute('data-remove-btn')) return;
+
+    const removedEl = target.closest('[data-todo-item]');
+    removedEl.remove();
+  };
+
+  const loadedHandler = () => {
+    const form = document.querySelector('#todoForm');
+    const todoContainer = document.querySelector('#todoItems');
+
+    form.addEventListener('submit', formHandler);
+    todoContainer.addEventListener('click', removeTodoItemHandler);
+  };
+
+  document.addEventListener('DOMContentLoaded', loadedHandler);
+})();
--- js/script.js
+++ js/script.js
@@ -1,73 +1,153 @@
-(() => {
-  const createTemplate = (data) => {
-    const wrapper = document.createElement('div');
-    wrapper.className = 'col-4';
-    wrapper.setAttribute('data-todo-item', '');
+(function () {
+  const CONSTANTS = Object.freeze({
+    todoFormSelector: '#todoForm',
+    todoContainerSelector: '#todoItems',
+    dataKey: 'formData',
+  });
 
-    const taskWrapper = document.createElement('div');
-    taskWrapper.className = 'taskWrapper';
-    wrapper.appendChild(taskWrapper);
+  const model = {
+    currentId: 0,
 
-    const deleteBtn = document.createElement('button');
-    deleteBtn.className = 'btn btn-sm btn-danger';
-    deleteBtn.textContent = 'x';
-    deleteBtn.setAttribute('data-remove-btn', '');
-    taskWrapper.appendChild(deleteBtn);
+    save(data) {
+      this.currentId += 1;
+      const dataCopy = { id: this.currentId, ...data };
+      const savedData = this.get();
+      savedData.push(dataCopy);
 
-    const taskHead = document.createElement('div');
-    taskHead.className = 'taskHeading';
-    taskHead.textContent = data.title;
-    taskWrapper.appendChild(taskHead);
+      try {
+        localStorage.setItem(CONSTANTS.dataKey, JSON.stringify(savedData));
+        return this.get().at(-1);
+      } catch (e) {
+        return false;
+      }
+    },
 
-    const taskDesc = document.createElement('div');
-    taskDesc.className = 'taskDescription';
-    taskDesc.textContent = data.description;
-    taskWrapper.appendChild(taskDesc);
+    get() {
+      const savedData = JSON.parse(localStorage.getItem(CONSTANTS.dataKey));
+      return savedData || [];
+    },
 
-    return wrapper;
+    removeElementById(todoId) {
+      const savedElements = this.get();
+      const index = savedElements.findIndex(({ id }) => todoId === id);
+      const [removedElement] = savedElements.splice(index, 1);
+      try {
+        localStorage.setItem(CONSTANTS.dataKey, JSON.stringify(savedElements));
+        return removedElement;
+      } catch (e) {
+        console.log('Cannot remove element', removedElement);
+        return false;
+      }
+    },
+
+    initId() {
+      const items = this.get();
+      if (!items.length) return;
+      this.currentId = +items.at(-1).id;
+    },
   };
 
-  const renderTodoItem = (elementToRender) => {
-    const todoContainer = document.querySelector('#todoItems');
-    todoContainer.prepend(elementToRender);
+  const view = {
+    renderElement(data) {
+      const template = this.createTemplate(data);
+      this.renderTodoItem(template);
+    },
 
-    return elementToRender;
+    renderTodoItem(elementToRender) {
+      const todoContainer = document.querySelector('#todoItems');
+      todoContainer.prepend(elementToRender);
+      return elementToRender;
+    },
+
+    createTemplate(data) {
+      const wrapper = document.createElement('div');
+      wrapper.className = 'col-4';
+      wrapper.setAttribute('data-todo-item', data.id);
+
+      const taskWrapper = document.createElement('div');
+      taskWrapper.className = 'taskWrapper';
+      wrapper.appendChild(taskWrapper);
+
+      const taskHeading = document.createElement('div');
+      taskHeading.className = 'taskHeading';
+      taskHeading.innerHTML = data.title;
+      taskWrapper.appendChild(taskHeading);
+
+      const taskDescription = document.createElement('div');
+      taskDescription.className = 'taskDescription';
+      taskDescription.innerHTML = data.description;
+      taskWrapper.appendChild(taskDescription);
+
+      const deleteBtn = document.createElement('button');
+      deleteBtn.className = 'btn btn-sm btn-danger';
+      deleteBtn.innerText = 'X';
+      deleteBtn.setAttribute('data-remove-btn', '');
+      taskWrapper.appendChild(deleteBtn);
+
+      return wrapper;
+    },
+    resetForm() {
+      document.querySelector(CONSTANTS.todoFormSelector).reset();
+    },
+
+    removeElement(todoId) {
+      document.querySelector(`[data-todo-item="${todoId}"]`).remove();
+    },
   };
 
-  const formHandler = (e) => {
-    e.preventDefault();
-    e.stopPropagation();
-    const { target } = e;
+  const controller = {
+    formHandler(e) {
+      e.preventDefault();
+      e.stopPropagation();
+      const { target } = e;
+      const data = Array.from(target.querySelectorAll('input, textarea'))
+        .reduce((acc, item) => {
+          acc[item.name] = item.value;
+          return acc;
+        }, {});
 
-    const inputData = {};
+      const savedData = model.save(data);
 
-    const inputs = target.querySelectorAll('input, textarea');
-    inputs.forEach((input) => {
-      if (input.value !== '') inputData[input.name] = input.value;
-    });
+      if (savedData) {
+        view.renderElement(savedData);
+        view.resetForm();
+      }
+    },
 
-    const HTMLTemplate = createTemplate(inputData);
-    renderTodoItem(HTMLTemplate);
-    target.reset();
+    removeTodoItemHandler(e) {
+      e.stopPropagation();
+      const { target } = e;
+      if (!target.hasAttribute('data-remove-btn')) return;
+      const todoId = +target.closest('[data-todo-item]').getAttribute('data-todo-item');
+      const removedElement = model.removeElementById(todoId);
+
+      if (removedElement) {
+        view.removeElement(todoId);
+        return;
+      }
+
+      alert(`Cannot remove element ${removedElement.title}`);
+    },
+
+    loadedHandler() {
+      model.initId();
+      const form = document.querySelector(CONSTANTS.todoFormSelector);
+      form.addEventListener('submit', this.formHandler);
+
+      model.get().forEach((item) => view.renderElement(item));
+
+      const todoContainer = document.querySelector(CONSTANTS.todoContainerSelector);
+      todoContainer.addEventListener('click', this.removeTodoItemHandler);
+    },
+
+    init() {
+      this.formHandler = this.formHandler.bind(this);
+      this.removeTodoItemHandler = this.removeTodoItemHandler.bind(this);
+      this.loadedHandler = this.loadedHandler.bind(this);
+
+      document.addEventListener('DOMContentLoaded', this.loadedHandler);
+    },
   };
 
-  const removeTodoItemHandler = (e) => {
-    e.stopPropagation();
-    const { target } = e;
-
-    if (!target.hasAttribute('data-remove-btn')) return;
-
-    const removedEl = target.closest('[data-todo-item]');
-    removedEl.remove();
-  };
-
-  const loadedHandler = () => {
-    const form = document.querySelector('#todoForm');
-    const todoContainer = document.querySelector('#todoItems');
-
-    form.addEventListener('submit', formHandler);
-    todoContainer.addEventListener('click', removeTodoItemHandler);
-  };
-
-  document.addEventListener('DOMContentLoaded', loadedHandler);
-})();
+  controller.init();
+}());
