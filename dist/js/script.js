471565e2f2782dbaf1daf41e04ee289c8a5169de
Ihor Troianovskyi <igtroya555@gmail.com>
2023-12-18 14:17:31 +0200

    hw 49 completed

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,8 +1,7 @@
 (() => {
   const userName = prompt('Input your name');
-  const age = prompt('Input your age');
   const greetings = (name) => {
-    alert(`Hello ${name}, you are ${age} y.o.`);
+    alert(`Hello ${name}`);
   };
 
   greetings(userName);
--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ const uglify = require('gulp-uglify');
 const cleanCSS = require('gulp-clean-css');
 const rename = require('gulp-rename');
 const sourcemaps = require('gulp-sourcemaps');
+const browserSync = require('browser-sync').create();
 
 const buildStyles = () => {
   return src('dist/scss/**/*.scss')
@@ -15,18 +16,20 @@ const buildStyles = () => {
     .pipe(cleanCSS({ compatibility: 'ie8' }))
     .pipe(rename({ suffix: '.min' }))
     .pipe(sourcemaps.write('./'))
-    .pipe(dest('build/styles'));
+    .pipe(dest('build/styles'))
+    .pipe(browserSync.stream());
 };
 
 const buildPug = () => {
   return src('dist/pages/*.pug')
     .pipe(pug())
-    .pipe(dest('build/'));
+    .pipe(dest('build/'))
+    .pipe(browserSync.stream());
 };
 
 const htmlCopy = () => {
   return src('dist/pages/*.html')
-    .pipe(dest('build/pages/'));
+    .pipe(dest('build/'));
 };
 
 const jsMinify = () => {
@@ -36,11 +39,15 @@ const jsMinify = () => {
     .pipe(dest('build/js/'));
 };
 
-const watchers = () => {
+const browserSyncJob = () => {
+  browserSync.init({
+    server: 'build/',
+  });
+
   watch('dist/scss/**/*.scss', buildStyles);
   watch('dist/pages/*.pug', buildPug);
   watch('dist/pages/*.html', htmlCopy);
   watch('dist/js/*js', jsMinify);
 };
 
-exports.watchers = watchers;
+exports.server = browserSyncJob;
